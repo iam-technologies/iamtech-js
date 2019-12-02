76bb1ac10e22e4c2df6a780807dba30e592e205d
Cristiam Da Silva <cristiamdasilva@Cristiams-MBP.home>
2019-12-02 11:23:39 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './scss/globals';
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
